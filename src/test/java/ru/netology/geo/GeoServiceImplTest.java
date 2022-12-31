fe27143fb6528ffe99977a91360132236f58d8d9
Vyacheslav Nechepurenko <masterdog8@mail.ru>
2022-12-31 12:30:23 +0300

    modification project commit

---
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -0,0 +1,16 @@
+package ru.netology.geo;
+
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+import ru.netology.entity.Location;
+
+public class GeoServiceImplTest {
+    @Test
+    void testByIp() {
+        GeoService geoService = new GeoServiceImpl();
+        Location actual = geoService.byIp("127.0.0.1");
+        Location expected = new Location(null, null, null, 0);
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
